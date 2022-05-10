6c5756ed916f50c3bfb2e288cd8657bb183e5532
sailesh.gopalakrishnan <sailsg111@gmail.com>
2022-05-10 15:45:51 +0800

    increased the grayscale and added a delay function so that it works on the project flux website

--- image_variation_extension/content.js
+++ image_variation_extension/content.js
@@ -2,16 +2,31 @@ console.log('Hello!')
 let imgs = document.getElementsByTagName("img");
 
 // let imagelink = 'images/cat1.jpg';
+function changeColorVariation() {
+    for (img of imgs) {
+        console.log(img.src)
 
-for (img of imgs) {
-    console.log(img.src)
+        // let file = imagelink
+        // let url = chrome.extension.getURL(file)
+        // img.src = url
+        // img.style.opacity = 0.5;
+        // img.style.filter = 'alpha(opacity=90)';
+        img.style.filter = 'contrast(80%) grayscale(80%) opacity(90%) saturate(18%)';
+        // console.log(url)
 
-    // let file = imagelink
-    // let url = chrome.extension.getURL(file)
-    // img.src = url
-    // img.style.opacity = 0.5;
-    // img.style.filter = 'alpha(opacity=90)';
-    img.style.filter = 'contrast(50%) grayscale(20%) opacity(80%) saturate(100%)';
-    // console.log(url)
-
+    }
 }
+setTimeout(changeColorVariation, 3000)
+// changeColorVariation()
+// for (img of imgs) {
+//     console.log(img.src)
+
+//     // let file = imagelink
+//     // let url = chrome.extension.getURL(file)
+//     // img.src = url
+//     // img.style.opacity = 0.5;
+//     // img.style.filter = 'alpha(opacity=90)';
+//     img.style.filter = 'contrast(80%) grayscale(80%) opacity(90%) saturate(18%)';
+//     // console.log(url)
+
+// }
