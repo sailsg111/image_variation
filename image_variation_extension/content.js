73046fb188f8751f3998fc307e7f8532faa29210
sailesh.gopalakrishnan <sailsg111@gmail.com>
2022-05-10 11:47:07 +0800

    added grayscale and contrast parameters in addition to opacity with reference to fakewebpage.html

--- image_variation_extension/content.js
+++ image_variation_extension/content.js
@@ -9,8 +9,9 @@ for (img of imgs) {
     // let file = imagelink
     // let url = chrome.extension.getURL(file)
     // img.src = url
-    img.style.opacity = 0.5;
-    img.style.filter = 'alpha(opacity=90)';
+    // img.style.opacity = 0.5;
+    // img.style.filter = 'alpha(opacity=90)';
+    img.style.filter = 'contrast(50%) grayscale(20%) opacity(80%) saturate(100%)';
     // console.log(url)
 
 }
